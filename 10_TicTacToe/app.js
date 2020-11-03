40caa30d36a96bec202b2a734b4dd5b0350166d2
“Wladiosan” <w.ohrimenko@gmail.com>
2020-11-03 19:48:07 +0200

    test

---
+++ 10_TicTacToe/app.js
@@ -0,0 +1,26 @@
+const mainBoxEl = document.getElementById('main-box')
+let step = 0
+
+const addListener = mainBoxEl.addEventListener('click', function (event) {
+    (step % 2 === 0) ? event.target.innerHTML = 'X' : event.target.innerHTML = 'O'
+    step++
+    check()
+})
+
+function check() {
+    let boxEl = document.getElementsByClassName('box')
+    let arr = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ]
+
+    for (var i = 0; i < arr.length; i++) {
+        if (boxEl[arr[i][0]].innerHTML === 'X' && boxEl[arr[i][1]].innerHTML === 'X' && boxEl[arr[i][2]].innerHTML === 'X') {
+            alert('Win X')
+        } else if (boxEl[arr[i][0]].innerHTML === 'O' && boxEl[arr[i][1]].innerHTML === 'O' && boxEl[arr[i][2]].innerHTML === 'O') {
+            alert('Win O')
+        }
+    }
+}
+
+
+
