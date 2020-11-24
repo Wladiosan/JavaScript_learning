d25b487660abc71e94e99d7e6896946971f259ed
“Wladiosan” <w.ohrimenko@gmail.com>
2020-11-24 21:50:48 +0200

    test

---
+++ PhoneApp/TodoApplication/src/screens/TodoScreen.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { StyleSheet, View, Button } from 'react-native'
+import { THEME } from '../theme'
+import { AppCard } from '../components/ui/AppCard'
+import { EditModal } from '../components/EditModal'
+import { AppTextBold } from '../components/ui/AppTextBold'
+import { AppButton } from '../components/ui/AppButton'
+
+export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
+  const [modal, setModal] = useState(false)
+
+  const saveHandler = title => {
+    onSave(todo.id, title)
+    setModal(false)
+  }
+
+  return (
+    <View>
+      <EditModal
+        value={todo.title}
+        visible={modal}
+        onCancel={() => setModal(false)}
+        onSave={saveHandler}
+      />
+
+      <AppCard style={styles.card}>
+        <AppTextBold style={styles.title}>{todo.title}</AppTextBold>
+        <Button title='Ред.' onPress={() => setModal(true)} />
+      </AppCard>
+
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <AppButton onPress={goBack} color={THEME.GREY_COLOR}>
+            Назад
+          </AppButton>
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Удалить'
+            color={THEME.DANGER_COLOR}
+            onPress={() => onRemove(todo.id)}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
+  button: {
+    width: '40%'
+  },
+  title: {
+    fontSize: 20
+  }
+})
